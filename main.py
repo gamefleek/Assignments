30ab3670fae1d4a7370ba050f82f1c9179c5ad01
Gamefleek <gamefleek@gmail.com>
2020-10-25 01:58:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#By Gamefleek 10/25/2020
+#Assignment 0.1
+
+male_sex = True
+age = 21
+
+if male_sex and age < 30:
+        print("you're still young, alfa.")
+if male_sex and age > 30:
+        print("you're pretty strong, alfa.")
+if not male_sex:
+    print("Welcome Teta.")
